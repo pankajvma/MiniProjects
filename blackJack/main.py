4716ccf409117fd8ea9de4312296103c066939de
pankajvma <pankajvma@gmail.com>
2021-03-16 09:58:02 +0530

    Added function compare_score() for better readability

--- blackJack/main.py
+++ blackJack/main.py
@@ -12,7 +12,7 @@ The the Ace can count as 11 or 1.
 
 cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'K', 'Q', 'A']
 
-def check_Score(card, score):
+def check_score(card, score):
   if card == 'A' and (score + 11) <= 21:
     score += 11
   elif card == 'A' and (score + 11) > 21:
@@ -28,78 +28,78 @@ def check_Score(card, score):
 def play_again():
   choice = input("Do you want to play again? Enter 'yes' or 'no': ").lower()
   if choice == 'yes':
-    start_BlackJack()
+    start_blackjack()
   else:
     print("Exting...")
 
-def start_BlackJack():
-  user_Deck = []
-  computer_Deck = []
-  user_Score = 0
-  computer_Score = 0  
+def compare_score(com_score, u_score):
+  if com_score == 21:
+    print("Computer've Won Already!")
+  elif u_score == 21:
+    print("You Won Already!")
+  elif com_score > u_score:
+    print(f"Computer have won with score {com_score}")
+  elif com_score < u_score:
+    print(f"You have won with score {u_score}")
+  else:
+    print("It's a draw")
+  play_again()
+
+def start_blackjack():
+  user_deck = []
+  computer_deck = []
+  user_score = 0
+  computer_score = 0  
 
   for _ in range(2):
-    computers_Card = random.choice(cards)
-    computer_Deck.append(computers_Card)
-    computer_Score = check_Score(computers_Card, computer_Score)
-    users_Card = random.choice(cards)
-    user_Deck.append(users_Card)
-    user_Score = check_Score(users_Card, user_Score)
+    computers_card = random.choice(cards)
+    computer_deck.append(computers_card)
+    computer_score = check_score(computers_card, computer_score)
+    users_card = random.choice(cards)
+    user_deck.append(users_card)
+    user_score = check_score(users_card, user_score)
 
-  if computer_Score == 21:
+  if computer_score == 21:
     print("Computer Won!")
+    play_again()
     return
   else:
-    print(f"Computer got: [{computer_Deck[0]}, ?]")
+    print(f"Computer got: [{computer_deck[0]}, ?]")
 
-  if user_Score == 21:
+  if user_score == 21:
     print("You Won Already!")
+    play_again()
     return
   else:
-    print(f"You've got: {user_Deck}")
+    print(f"You've got: {user_deck}")
     while True:
-      want_Another_Card = input("Do you want another card? Enter 'yes' or 'no': ").lower()
-      if want_Another_Card == 'yes':
-        users_Card = random.choice(cards)
-        user_Deck.append(users_Card)
-        user_Score = check_Score(users_Card, user_Score)
-        print(f"You've got: {user_Deck}")
-        print(f"Your score is: {user_Score}")
-        if user_Score == 21:
+      want_another_card = input("Do you want another card? Enter 'yes' or 'no': ").lower()
+      if want_another_card == 'yes':
+        users_card = random.choice(cards)
+        user_deck.append(users_card)
+        user_score = check_score(users_card, user_score)
+        print(f"You've got: {user_deck}")
+        print(f"Your score is: {user_score}")
+        if user_score == 21:
           print("You Won Already!")
           play_again()
           return
-        elif user_Score > 21:
+        elif user_score > 21:
           print("You Lose!")
           play_again()
           return
       else:
         break
 
-  while computer_Score < 17:
-    computers_Card = random.choice(cards)
-    computer_Deck.append(computers_Card)
-    computer_Score = check_Score(computers_Card, computer_Score)
+  while computer_score < 17:
+    computers_card = random.choice(cards)
+    computer_deck.append(computers_card)
+    computer_score = check_score(computers_card, computer_score)
 
-    print(f"Computer've got: {computer_Deck}")
-    print(f"Computer's score is: {computer_Score}")
+  print(f"Computer've got: {computer_deck}")
+  print(f"Computer's score is: {computer_score}")
 
-    if user_Score == 21:
-      print("Computer've Won Already!")
-      play_again()
-      return
-    elif user_Score > 21:
-      print("You Won Already!")
-      play_again()
-      return
+  compare_score(computer_score, user_score)
+  return
 
-  if computer_Score > user_Score:
-    print(f"Computer have won with score {computer_Score}")
-    play_again()
-    return
-  else:
-    print(f"You have won with score {user_Score}")
-    play_again()
-    return
-
-start_BlackJack()
+start_blackjack()

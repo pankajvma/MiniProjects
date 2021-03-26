6e85a042a6e94855e0508aee2c149e8db9cb0c33
pankajvma <pankajvma@gmail.com>
2021-03-26 13:38:48 +0530

    Added Grid-lines.

--- turtle_random_walk/README.md
+++ turtle_random_walk/README.md
@@ -1,5 +1,7 @@
 This is the output of one of the turtle's Randomwalks. Basic purpose of the mini-project is to see how often an object which is following random instructions, can retrace its own path when executing a given number of steps.
 
+Grid lines have been added for better visualization.
+
 Implemented using lists, randomization, and OOP.
 
 ![turtle_randomwallk](https://user-images.githubusercontent.com/47264501/112299700-0e81e200-8cbe-11eb-93f2-4db67dffbc3c.gif)
--- turtle_random_walk/main.py
+++ turtle_random_walk/main.py
@@ -2,16 +2,58 @@ from turtle import Turtle, Screen
 import random
 
 tuntun = Turtle()
-tuntun.pensize(10)
+tuntun.pensize(5)
 tuntun.speed("fast")
+tuntun.penup()
+tuntun.setposition(-10, -10)
+tuntun.pendown()
+
+screen = Screen()
+
+screen.setup(700, 700)
+
+graph_maker = Turtle()
+graph_maker.pensize(1)
+graph_maker.speed("fastest")
+graph_maker.penup()
+graph_maker.setposition(-350, -350)
+graph_maker.pendown()
+
+
+def draw_from(direction):
+    graph_maker.forward(700)
+    graph_maker.penup()
+    if direction == "left":
+        graph_maker.left(90)
+    else:
+        graph_maker.right(90)
+    graph_maker.forward(20)
+    graph_maker.pendown()
+    if direction == "left":
+        graph_maker.left(90)
+    else:
+        graph_maker.right(90)
+        
+
+def generate_graph():
+    for _ in range(17):
+        draw_from("left")
+        draw_from("right")
+
+    graph_maker.right(90)
+
+    for _ in range(18):
+        draw_from("left")
+        draw_from("right")
+
+generate_graph()
 
 turtle_colors = ["dark slate blue", "indigo", "dark violet", "dark magenta", "medium violet red", "maroon", 
                     "saddle brown", "dark green", "blue", "orange", "gold", "yellow", "olive drab", "red",
                     "slate gray", "gainsboro", "thistle", "dark goldenrod", "cyan", "navajo white", "dark olive green"]
 
-screen = Screen()
-
 turning_angle = [0, 90, 180, 270]
+
 
 for _ in range(200):
     tuntun.pencolor(random.choice(turtle_colors))

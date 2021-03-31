29f74411be7d4d7f1d293c515093869bf318a41e
pankajvma <pankajvma@gmail.com>
2021-03-31 18:10:26 +0530

    Created the turtle and also generrating random number of cars.

--- turtle-road-crossing/car_manager.py
+++ turtle-road-crossing/car_manager.py
@@ -1,7 +1,34 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+
+    def __init__(self):
+        self.cars = []
+        
+
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 6:
+            car = Turtle()
+            car.shape("square")
+            car.shapesize(1, 2)
+            car.penup()
+            car.color(random.choice(COLORS))
+            y = random.randint(-250, 250)
+            car.setposition(320, y)
+            car.setheading(180)
+            self.cars.append(car)
+
+    def drive(self):
+        for car in self.cars:
+            car.forward(STARTING_MOVE_DISTANCE)
+
+
+    
+
--- turtle-road-crossing/main.py
+++ turtle-road-crossing/main.py
@@ -8,7 +8,20 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
+screen.listen()
+screen.onkey(player.move_up, "Up")
+
+car = CarManager()
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car.create_car()
+    car.drive()
+
+    for cr in car.cars:
+        if cr.distance(player) < 20:
+            game_is_on = False

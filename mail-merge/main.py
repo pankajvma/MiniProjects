f785d8e4ee460c3ec76b3b4c80a7c50f37ec0150
pankajvma <pankajvma@gmail.com>
2021-04-03 16:35:19 +0530

    Created Minor Project Male Merge, to generate Mails/letters.

---
+++ mail-merge/main.py
@@ -0,0 +1,27 @@
+invited_names_path = './input/names/invited_names.txt'
+letter_path = './input/letters/meeting_invitation.txt'
+invited_names = []
+output_letters = './output/'
+
+with open(invited_names_path) as reader:
+        invited_names = reader.readlines()
+
+with open(letter_path) as reader:
+        letter = reader.read()
+
+
+new_invited_names = []
+for name in invited_names:
+    new_invited_names.append(name.strip('\n'))
+
+
+new_letter = ''
+
+for name in new_invited_names:
+    new_letter = letter.replace('[name]', name)
+    file_name = name + ".txt"
+    with open(output_letters + file_name, 'w') as writer:
+        writer.write(new_letter)
+
+
+print("Task Successful.  Please check the output folder to find the generated mails.")

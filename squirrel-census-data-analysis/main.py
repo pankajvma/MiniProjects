6b8a68e6118c7633b62541910d0d0ad6721aa2c5
pankajvma <pankajvma@gmail.com>
2021-04-06 14:55:15 +0530

    Generated census report

--- squirrel-census-data-analysis/main.py
+++ squirrel-census-data-analysis/main.py
@@ -5,10 +5,21 @@ squirrel_census_data = pd.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel
 # print(squirrel_census_data)
 attribute_to_check = 'Primary Fur Color'
 
+total_squirrels = squirrel_census_data[attribute_to_check]
 gray_squirrels = squirrel_census_data[squirrel_census_data[attribute_to_check] == 'Gray']
 cinnamon_squirrels = squirrel_census_data[squirrel_census_data[attribute_to_check] == 'Cinnamon']
 black_squirrels = squirrel_census_data[squirrel_census_data[attribute_to_check] == 'Black']
 
-print(f"Gray: {len(gray_squirrels)}")
-print(f"Cinnamon: {len(cinnamon_squirrels)}")
-print(f"Black: {len(black_squirrels)}")
+# print(f"Gray squirrels: {len(gray_squirrels)}")
+# print(f"Cinnamon squirrels: {len(cinnamon_squirrels)}")
+# print(f"Black squirrels: {len(black_squirrels)}")
+# print(f"Total squirrels in the central park: {len(total_squirrels)}")
+
+data_dict = {
+    "Fur color": ['Gray', 'Cinnamon', 'Black'],
+    "Squirrel count": [len(gray_squirrels), len(cinnamon_squirrels), len(black_squirrels)]
+}
+
+# Creating and saving dataframe as CSV
+df = pd.DataFrame(data_dict)
+df.to_csv('squirrel_count_data_frame.csv')

9855d24d7c3d9accf6f8f5e6c785b5af5f864957
pankajvma <pankajvma@gmail.com>
2021-07-01 15:04:52 +0530

    Handled exception

--- pandas-nato-alphabet/main.py
+++ pandas-nato-alphabet/main.py
@@ -2,13 +2,19 @@ import pandas as pd
 
 df = pd.read_csv('nato_phonetic_alphabet.csv')
 
-name = input("Enter your name: ")
-print("\nHere are the NATO alphabets corresponding to exact letter in your name.\n")
+def nato_phone():
+    name = input("Enter your name: ")
+    print("\nHere are the NATO alphabets corresponding to exact letter in your name.\n")
 
-phonetic_dictionary = {row.letter:row.code for (index, row) in df.iterrows()}
+    phonetic_dictionary = {row.letter:row.code for (index, row) in df.iterrows()}
 
+    try:
+        phonetic_words = [f"{letter}: {phonetic_dictionary[letter]}" for letter in name.upper()]
+    except KeyError:
+        print('Sorry, only letters in the alphabets please...')
+        nato_phone()
+    else:
+        for word in phonetic_words:
+            print(word)
 
-phonetic_words = [f"{letter}: {phonetic_dictionary[letter]}" for letter in name.upper()]
-
-for word in phonetic_words:
-    print(word)
+nato_phone()
